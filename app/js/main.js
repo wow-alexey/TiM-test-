52c12dbfbf8bc4b2c916ba003d9fa0c4efa7c55b
Alexey Dmytrenko <wow-alexey@mail.ru>
2017-05-04 20:17:28 +0300

    second commit

---
+++ app/js/main.js
@@ -0,0 +1,7 @@
+$('.corusel').slick({
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    nextArrow: '<button type="button" data-role="none" class="slick-next" aria-label="Next" tabindex="0" role="button"><i class="fa fa-angle-right" aria-hidden="true"></i></button>',
+    prevArrow: '<button type="button" data-role="none" class="slick-prev" aria-label="Previous" tabindex="0" role="button"><i class="fa fa-angle-left" aria-hidden="true"></i></button>'
+});
